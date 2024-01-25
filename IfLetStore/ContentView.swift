fe6f723c995539c1bafd5387731d84e4a74aea20
Minho Yi <minho.yi@riiid.co>
2024-01-25 10:33:30 +0900

    🎉 init project

---
+++ IfLetStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IfLetStore
+//
+//  Created by Minho Yi on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IfLetStore/IfLetStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  IfLetStoreApp.swift
+//  IfLetStore
+//
+//  Created by Minho Yi on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct IfLetStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
